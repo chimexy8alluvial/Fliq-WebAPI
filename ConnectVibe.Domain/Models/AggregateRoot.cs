3d451bf7fa545d3717b2bef05f9b18d3dc902263
kellyp2 <121811284+kellyp2@users.noreply.github.com>
2024-08-06 15:42:13 +0100

    Aggregate, root, Entity, valueobject

---
+++ ConnectVibe.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace ConnectVibe.Domain.Models
+{
+    public class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id)
+        {
+
+        }
+    }
+}
