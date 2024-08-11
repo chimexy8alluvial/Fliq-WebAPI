4d61b52ae64cc1f4303d09c9a6e8640c80f76034
kellyp2 <121811284+kellyp2@users.noreply.github.com>
2024-08-11 19:45:06 +0100

    swagger update 2

--- ConnectVibe.Api/Values.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConnectVibe.Api
-{
-    public class Values
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public DateTime Date { get; set; }
-    }
-}
