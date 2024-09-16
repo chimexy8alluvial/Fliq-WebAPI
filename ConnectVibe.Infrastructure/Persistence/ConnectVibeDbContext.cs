bf9d6ac114cc50bf528b722c189f9fdd9114cdff
mandyke <dike.ogwuma3@gmail.com>
2024-09-16 16:53:35 +0100

    Third Commit

--- ConnectVibe.Infrastructure/Persistence/ConnectVibeDbContext.cs
+++ ConnectVibe.Infrastructure/Persistence/ConnectVibeDbContext.cs
@@ -15,7 +15,12 @@ namespace ConnectVibe.Infrastructure.Persistence
         public DbSet<OTP> OTPs { get; set; } = null!;
         public DbSet<UserProfile> UserProfiles { get; set; } = null!;
         public DbSet<LocationDetail> LocationDetails { get; set; }
-        public DbSet<Events> events { get; set; }
+        public DbSet<Events> Events { get; set; }
+        public DbSet<EventMedia> EventMedias { get; set; }
+        public DbSet<EventCriteria> EventCriterias { get; set; }
+        public DbSet<SponsoredEventDetail> SponsoredEventDetails { get; set; }
+        public DbSet<TicketType> TicketTypes { get; set; }
+
        
     } 
 };
