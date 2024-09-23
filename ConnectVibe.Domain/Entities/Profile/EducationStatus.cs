3080b5ddb937868614de35753162821cddfaa7ea
La0lu <oguntoyetobi456@gmail.com>
2024-09-23 15:21:16 +0100

    updates to user profile entity

---
+++ ConnectVibe.Domain/Entities/Profile/EducationStatus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fliq.Domain.Entities.Profile
+{
+    public class EducationStatus
+    {
+        public int Id { get; set; }
+        public EducationLevel EducationLevel { get; set; }
+        public bool IsVisible { get; set; }
+    }
+
+    public enum EducationLevel
+    {
+        HighSchool,
+        AssociateDegree,
+        BachelorDegree,
+        MasterDegree,
+        Doctorate,
+        Other,
+        PreferNotToSay
+    }
+}
