0c27e749525a305c4337a0f5f4a8883d450607a7
La0lu <oguntoyetobi456@gmail.com>
2024-09-23 15:46:20 +0100

    update request Dto for profile creation

--- ConnectVibe.Contracts/Profile/CreateProfileRequest.cs
+++ ConnectVibe.Contracts/Profile/CreateProfileRequest.cs
@@ -1,14 +1,19 @@
-﻿namespace ConnectVibe.Contracts.Profile
+﻿using Fliq.Contracts.Profile;
+
+namespace ConnectVibe.Contracts.Profile
 {
     public record CreateProfileRequest(
     int UserId,
      DateTime DOB,
       List<string> Passions,
     List<ProfilePhotoDto> Photos,
+    List<ProfileTypeDto> profileTypes,
      GenderDto Gender,
      SexualOrientationDto SexualOrientation,
      ReligionDto Religion,
      EthnicityDto Ethnicity,
+     OccupationDto Occupation,
+     EducationStatusDto EducationStatus,
      HaveKidsDto HaveKids,
      WantKidsDto WantKids,
      LocationDto Location,
--- ConnectVibe.Domain/Entities/Profile/UserProfile.cs
+++ ConnectVibe.Domain/Entities/Profile/UserProfile.cs
@@ -17,9 +17,9 @@ namespace ConnectVibe.Domain.Entities.Profile
         public WantKids? WantKids { get; set; }  //Nullable
         public Location Location { get; set; } = default!;
         public bool AllowNotifications { get; set; }
-        public List<string> Passions { get; set; } = default!;
-        public List<ProfilePhoto> Photos { get; set; } = default!;
-        public List<ProfileType> ProfileTypes { get; set; } = [];
+        public List<string> Passions { get; set; } = new();
+        public List<ProfilePhoto> Photos { get; set; } = new();
+        public List<ProfileType> ProfileTypes { get; set; } = new();
         public int UserId { get; set; }
         public User User { get; set; } = default!;
     }
