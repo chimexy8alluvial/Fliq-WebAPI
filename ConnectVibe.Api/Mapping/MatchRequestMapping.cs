0676655d9ce084fd273b90033cb026d5644f28e3
Dike <dike.ogwuma3@gmail.com>
2024-10-24 08:09:52 +0100

    Code review

---
+++ ConnectVibe.Api/Mapping/MatchRequestMapping.cs
@@ -0,0 +1,14 @@
+﻿using Fliq.Application.MatchedProfile.Commands.MatchedList;
+using Fliq.Application.MatchedProfile.Common;
+using Mapster;
+
+namespace Fliq.Api.Mapping
+{
+    public class MatchRequestMapping : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<CreateMatchListResult, GetMatchRequestListCommand>().IgnoreNullValues(true);
+        }
+    }
+}
