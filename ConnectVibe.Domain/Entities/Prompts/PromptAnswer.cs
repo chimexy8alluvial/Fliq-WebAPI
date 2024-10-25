54c37a113e018496f273c9599266716e8cd8b1ad
La0lu <oguntoyetobi456@gmail.com>
2024-10-25 06:59:23 +0100

    Prompt models defined

---
+++ ConnectVibe.Domain/Entities/Prompts/PromptAnswer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fliq.Domain.Entities.Prompts
+{
+    public class PromptAnswer : Record
+    {
+        public int PromptQuestionId { get; set; }
+        public PromptQuestion PromptQuestion { get; set; } = default!;
+        public string AnswerText { get; set; } = default!;
+        public string VoiceNoteUrl { get; set; } = default!;
+        public string VideoClipUrl { get; set; } = default!;
+        public int UserId { get; set; }
+    }
+}
---
+++ ConnectVibe.Domain/Entities/Prompts/PromptQuestion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fliq.Domain.Entities.Prompts
+{
+    public class PromptQuestion : Record
+    {
+        public string QuestionText { get; set; } = default!;
+        public bool IsSystemGenerated { get; set; }
+        public int PromptCategoryId { get; set; }
+        public PromptCategory PromptCategory { get; set; } = default!;
+        public ICollection<PromptAnswer> PromptAnswers { get; set; } = new List<PromptAnswer>();
+
+    }
+}
