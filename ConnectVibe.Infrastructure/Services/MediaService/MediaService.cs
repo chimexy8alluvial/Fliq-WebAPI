026d3488e87e0cd48bd2e98cb0ca13f077c4b76e
La0lu <oguntoyetobi456@gmail.com>
2024-11-23 13:46:59 +0100

    Added media service

---
+++ ConnectVibe.Infrastructure/Services/MediaService/MediaService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+
+
+namespace Fliq.Infrastructure.Services.MediaService
+{
+    public class MediaService
+    {
+        private readonly string _uploadPath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
+        private readonly string _containerName = "documents";
+
+
+        public async Task<string?> UploadEventMediaAsync(IFormFile mediaUpload)
+        {
+            // Saving the Media to a Local Directory
+            if (mediaUpload == null || mediaUpload.Length == 0)
+            {
+                return null;
+            }
+
+            var filePath = Path.Combine(_uploadPath, mediaUpload.FileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await mediaUpload.CopyToAsync(stream);
+            }
+
+            var fileUrl = filePath;
+            return fileUrl;
+        }
+    }
+}
