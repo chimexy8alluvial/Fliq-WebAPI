022ae32ac7725ccb8a93d248e2c6cde0a5b143f8
La0lu <oguntoyetobi456@gmail.com>
2024-11-23 13:49:39 +0100

    Add media interface

---
+++ ConnectVibe.Application/Common/Interfaces/Services/MeidaServices/IMediaServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fliq.Application.Common.Interfaces.Services.MeidaServices
+{
+    internal interface IMediaServices
+    {
+    }
+}
