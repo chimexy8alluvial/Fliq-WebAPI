a36404f6797c2d11d137e52578afd3afeab68380
La0lu <oguntoyetobi456@gmail.com>
2025-02-14 07:03:25 +0100

    Updated user entity with IsActive property

---
+++ ConnectVibe.Infrastructure/Migrations/20250214054751_AddIsActiveToUser.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fliq.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsActiveToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsResolved",
+                table: "Stakes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResolutionOption",
+                table: "Stakes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsResolved",
+                table: "Stakes");
+
+            migrationBuilder.DropColumn(
+                name: "ResolutionOption",
+                table: "Stakes");
+        }
+    }
+}
