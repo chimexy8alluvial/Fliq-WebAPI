48762aa28e8567af8c37573c8bd7643f4167ba04
La0lu <oguntoyetobi456@gmail.com>
2025-02-14 09:41:01 +0100

    updated UpdateUserLastActive stored procedure to update Active status as well

---
+++ ConnectVibe.Infrastructure/Migrations/20250214083526_Update01_sp_UpdateUserLastActiveAt.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fliq.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update01_sp_UpdateUserLastActiveAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            ALTER PROCEDURE sp_UpdateUserLastActiveAt
+                @UserId INT,
+                @LastActiveAt DATETIME
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+                
+                UPDATE Users
+                SET LastActiveAt = @LastActiveAt,
+                    IsActive = 1
+                WHERE Id = @UserId;
+            END;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            ALTER PROCEDURE sp_UpdateUserLastActiveAt
+                @UserId INT,
+                @LastActiveAt DATETIME
+            AS
+            BEGIN
+                SET NOCOUNT ON;
+                
+                UPDATE Users
+                SET LastActiveAt = @LastActiveAt
+                WHERE Id = @UserId;
+            END;
+            ");
+        }
+    }
+}
