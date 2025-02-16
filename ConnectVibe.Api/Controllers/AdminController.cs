24dc20e4896dcb561e090f350a9aa1c3d5fcbcbb
La0lu <oguntoyetobi456@gmail.com>
2025-02-16 11:42:21 +0100

    Admin creation endpoint

---
+++ ConnectVibe.Api/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using Fliq.Application.Authentication.Commands.CreateAdmin;
+using Fliq.Application.Common.Interfaces.Persistence;
+using Fliq.Application.Common.Interfaces.Services;
+using Fliq.Contracts.Authentication;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fliq.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ApiBaseController
+    {
+        private readonly ISender _mediator;
+        private readonly IMapper _mapper;
+        private readonly ILoggerManager _logger;
+
+        public AdminController(ISender mediator, IMapper mapper, ILoggerManager logger)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        [Authorize(Roles = "SuperAdmin")]
+        [HttpPost("create-admin")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            _logger.LogInfo($"Admin Creation Request Received: {request}");
+            var command = _mapper.Map<CreateAdminCommand>(request);
+            var authResult = await _mediator.Send(command);
+            _logger.LogInfo($"Create Admin Command Executed. Result: {authResult}");
+            return authResult.Match(
+                authResult => Ok(_mapper.Map<RegisterResponse>(authResult)),
+                errors => Problem(errors)
+            );
+        }
+    }
+}
