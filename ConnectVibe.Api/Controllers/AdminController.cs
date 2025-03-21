0f4cbb542f46e507288235cded23df3dbf568596
La0lu <oguntoyetobi456@gmail.com>
2025-03-21 07:53:30 +0100

    DEactivate user done

--- ConnectVibe.Api/Controllers/AdminController.cs
+++ ConnectVibe.Api/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
 ﻿using Fliq.Application.Authentication.Commands.CreateAdmin;
 using Fliq.Application.Common.Interfaces.Services;
+using Fliq.Application.Users.Commands;
 using Fliq.Contracts.Authentication;
+using Fliq.Contracts.Common;
+using Fliq.Contracts.Users;
 using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -25,7 +28,7 @@ namespace Fliq.Api.Controllers
 
         [Authorize(Roles = "SuperAdmin")]
         [HttpPost("create-admin")]
-        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        public async Task<IActionResult> CreateAdmin([FromBody] RegisterRequest request)
         {
             _logger.LogInfo($"Admin Creation Request Received: {request}");
             var command = _mapper.Map<CreateAdminCommand>(request);
@@ -33,6 +36,22 @@ namespace Fliq.Api.Controllers
             _logger.LogInfo($"Create Admin Command Executed. Result: {authResult}");
             return authResult.Match(
                 authResult => Ok(_mapper.Map<RegisterResponse>(authResult)),
+                errors => Problem(errors)
+            );
+        }
+
+
+        [Authorize(Roles = "SuperAdmin")]
+        [HttpPost("deactivate-user/{UserId}")]
+        public async Task<IActionResult> DeactivateUser(int UserId)
+        {
+            _logger.LogInfo($"User Deactivation Request Received for User with Id: {UserId}");
+            var command = new DeactivateUserCommand(UserId);
+            var result = await _mediator.Send(command);
+            _logger.LogInfo($"User Deactivation Command Executed. Result:  {result}");
+
+            return result.Match(
+                result => Ok(new BasicActionResponse($"User with ID {UserId} deactivated successfully")),
                 errors => Problem(errors)
             );
         }
