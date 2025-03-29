d05adcd23e8afed2e5306eb89b2cd404d0073e27
Africa Agwu <agwuafricaagwu@gmail.com>
2025-03-29 18:40:44 +0100

    Update to registeration

--- ConnectVibe.Domain/Entities/Settings/Setting.cs
+++ ConnectVibe.Domain/Entities/Settings/Setting.cs
@@ -1,11 +1,13 @@
-﻿namespace Fliq.Domain.Entities.Settings
+﻿using Fliq.Domain.Enums;
+
+namespace Fliq.Domain.Entities.Settings
 {
     public class Setting : Record
     {
         public ScreenMode ScreenMode { get; set; }
         public bool RelationAvailability { get; set; }
         public bool ShowMusicAndGameStatus { get; set; }
-        public string Language { get; set; } = "English";
+        public Language Language { get; set; } = Language.English;
         public ICollection<NotificationPreference> NotificationPreferences { get; set; } = new List<NotificationPreference>();
         public Filter Filter { get; set; } = new Filter();
         public User User { get; set; } = default!;
---
+++ ConnectVibe.Infrastructure/Migrations/20250329173259_phonenumber.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fliq.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class phonenumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Language",
+                table: "Settings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2025, 3, 29, 17, 32, 57, 360, DateTimeKind.Utc).AddTicks(7895));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateCreated",
+                value: new DateTime(2025, 3, 29, 17, 32, 57, 360, DateTimeKind.Utc).AddTicks(7900));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DateCreated",
+                value: new DateTime(2025, 3, 29, 17, 32, 57, 360, DateTimeKind.Utc).AddTicks(7901));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Language",
+                table: "Settings",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2025, 3, 28, 8, 18, 48, 936, DateTimeKind.Utc).AddTicks(2421));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateCreated",
+                value: new DateTime(2025, 3, 28, 8, 18, 48, 936, DateTimeKind.Utc).AddTicks(2435));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DateCreated",
+                value: new DateTime(2025, 3, 28, 8, 18, 48, 936, DateTimeKind.Utc).AddTicks(2437));
+        }
+    }
+}
