a3c134924defe5c3aa1b17d668c5ca28254905d9
wereuche <66272093+wereuche@users.noreply.github.com>
2025-04-01 16:07:59 +0100

    mapped evented tickets

---
+++ ConnectVibe.Api/Mapping/DashBoardEventMappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using Fliq.Application.DashBoard.Common;
+using Fliq.Contracts.DashBoard;
+using Mapster;
+
+namespace Fliq.Api.Mapping
+{
+    public class DashBoardEventMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<GetEventsTicketsResult, GetEventsTicketsResponse>();
+        }
+    }
+}
