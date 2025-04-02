4ec5bc6fe5bcfafda366a94b1502a2b5ea910187
wereuche <66272093+wereuche@users.noreply.github.com>
2025-04-02 15:19:03 +0100

    refunded

---
+++ ConnectVibe.Infrastructure/Migrations/20250402135414_IsRefundedToTicket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fliq.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsRefundedToTicket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRefunded",
+                table: "Tickets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRefunded",
+                table: "Tickets");
+        }
+    }
+}
