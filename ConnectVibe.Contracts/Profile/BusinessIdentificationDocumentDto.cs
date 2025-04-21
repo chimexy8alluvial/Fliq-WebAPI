e8a169cb4d2a09179bf0fc7ef2e4379d11efa243
Chumexxx <obasyemeka@gmail.com>
2025-04-21 11:47:39 +0100

    update

--- ConnectVibe.Contracts/Profile/BusinessIdentificationDocumentDto.cs
+++ ConnectVibe.Contracts/Profile/BusinessIdentificationDocumentDto.cs
@@ -7,7 +7,7 @@ namespace Fliq.Contracts.Profile
     public class BusinessIdentificationDocumentDto
     {
         public int BusinessIdentificationDocumentTypeId { get; set; } = default!;
-        public BusinessIdentificationDocumentType? BusinessIdentificationDocumentType = default!;
+        public BusinessIdentificationDocumentType? BusinessIdentificationDocumentType  = default!;
         public IFormFile? BusinessIdentificationDocumentFront { get; set; } = default!;
         public IFormFile? BusinessIdentificationDocumentBack { get; set; } = default!;
     }
