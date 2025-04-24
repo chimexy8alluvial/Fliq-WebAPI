2b2d0a2f082f512ba0be8871c92dc9059c9bbb00
La0lu <oguntoyetobi456@gmail.com>
2025-04-24 17:57:03 +0100

    Profile fixes

--- ConnectVibe.Contracts/Profile/UpdateDtos/UpdateEthnicityDto.cs
+++ ConnectVibe.Contracts/Profile/UpdateDtos/UpdateEthnicityDto.cs
@@ -1,4 +1,6 @@
 ﻿namespace Fliq.Contracts.Profile.UpdateDtos
 {
     public record UpdateEthnicityDto(int Id, int? EthnicityType, bool? IsVisible);
+
+    public record ReadEthnicityDto(int Id, string? EthnicityType, bool? IsVisible);
 }
--- ConnectVibe.Contracts/Profile/UpdateDtos/UpdateWantKidsDto.cs
+++ ConnectVibe.Contracts/Profile/UpdateDtos/UpdateWantKidsDto.cs
@@ -1,4 +1,5 @@
 ﻿namespace Fliq.Contracts.Profile.UpdateDtos
 {
     public record UpdateWantKidsDto(int Id, int? WantKidsType, bool? IsVisible);
+    public record ReadWantKidsDto(int Id, string? WantKidsType, bool? IsVisible);
 }
