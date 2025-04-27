efe1095e13539b5691a77fe5b581156f05f9311f
Africa Agwu <agwuafricaagwu@gmail.com>
2025-04-27 07:41:45 +0100

    Update to settings

---
+++ ConnectVibe.Infrastructure/Migrations/20250427063641_theme_modification.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fliq.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class theme_modification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ScreenMode",
+                table: "Settings",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DisconnectionResolutionOption",
+                table: "GameSessions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WinnerId",
+                table: "GameSessions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GameDisconnectionResolutionOption",
+                table: "GameRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisconnectionResolutionOption",
+                table: "GameSessions");
+
+            migrationBuilder.DropColumn(
+                name: "WinnerId",
+                table: "GameSessions");
+
+            migrationBuilder.DropColumn(
+                name: "GameDisconnectionResolutionOption",
+                table: "GameRequests");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Language",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ScreenMode",
+                table: "Settings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
