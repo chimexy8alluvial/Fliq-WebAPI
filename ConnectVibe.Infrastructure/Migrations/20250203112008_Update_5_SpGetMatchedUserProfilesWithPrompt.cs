d98b1c1fcc7d9875266e6e93388abb88b5500548
wereuche <66272093+wereuche@users.noreply.github.com>
2025-05-12 11:59:15 +0100

    event explore

--- ConnectVibe.Infrastructure/Migrations/20250203112008_Update_5_SpGetMatchedUserProfilesWithPrompt.cs
+++ ConnectVibe.Infrastructure/Migrations/20250203112008_Update_5_SpGetMatchedUserProfilesWithPrompt.cs
@@ -12,107 +12,104 @@ namespace Fliq.Infrastructure.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-            ALTER   PROCEDURE [dbo].[sPGetMatchedUserProfiles]
-                @userId INT,
-                @profileTypes NVARCHAR(MAX),  
-                @filterByDating BIT = NULL,
-                @filterByFriendship BIT = NULL,
-                @pageNumber INT = 1,
-                @pageSize INT
-            AS
+            CREATE OR ALTER PROCEDURE [dbo].[sPGetMatchedUserProfiles]
+            @userId INT,
+            @profileTypes NVARCHAR(MAX),  
+            @filterByDating BIT = NULL,
+            @filterByFriendship BIT = NULL,
+            @pageNumber INT = 1,
+            @pageSize INT
+        AS
+        BEGIN
+            SET NOCOUNT ON;
+
+            DECLARE @from_row INT = 1;
+            IF @pageNumber > 1
             BEGIN
-                DECLARE @from_row INT = 1;
-                IF @pageNumber > 1
-                BEGIN
-                    SET @from_row = ((@pageNumber * @pageSize) - (@pageSize)) + 1;
-                END;
-            
-                WITH UserProfileRecords AS
-                (
-                    SELECT 
-                        up.UserId, 
-                        up.DOB, 
-                        g.Id AS GenderId, 
-                        g.GenderType, 
-                        g.IsVisible AS GenderVisible,
-            			oc.Id AS OccupationId,
-            			oc.OccupationName AS OccupationName,
-            			oc.IsVisible AS OccupationVisible,
-            			ed.Id AS EducationStatusId,
-            			ed.EducationLevel AS EducationLevel,
-            			ed.IsVisible AS EducationVisible,
-                        so.Id AS SexualOrientationId, 
-                        so.SexualOrientationType, 
-                        so.IsVisible AS SexualOrientationVisible, 
-                        r.Id AS ReligionId, 
-                        r.ReligionType, 
-                        r.IsVisible AS ReligionVisible, 
-                        e.Id AS EthnicityId, 
-                        e.EthnicityType, 
-                        e.IsVisible AS EthnicityVisible, 
-                        hk.Id AS HaveKidsId, 
-                        hk.HaveKidsType, 
-                        hk.IsVisible AS HaveKidsVisible, 
-                        wk.Id AS WantKidsId, 
-                        wk.WantKidsType, 
-                        wk.IsVisible AS WantKidsVisible, 
-                        loc.Id AS LocationId, 
-                        loc.Lat, 
-                        loc.Lng, 
-                        loc.IsVisible AS LocationVisible, 
-                        up.AllowNotifications,
-                        up.Passions,
-            			up.ProfileTypes,
-                        up.DateCreated,
-            			up.DateModified,
-            			up.IsDeleted,
-            			up.Id,
-						  
-                  (
-                    SELECT 
-                        pr.Id,
-                        pr.ResponseType,
-						pr.Response,
-					    pr.PromptQuestionId
-                    FROM dbo.PromptResponse pr
-                    WHERE pr.UserProfileId = up.Id
-                    FOR JSON PATH
-                ) AS PromptResponses,
-                        ROW_NUMBER() OVER (ORDER BY up.DateCreated DESC) AS Row_Num
-                    FROM dbo.UserProfiles up
-                    
-                    LEFT JOIN Gender g ON up.GenderId = g.Id
-            		LEFT JOIN Occupation oc ON up.OccupationId = oc.Id
-            		LEFT JOIN EducationStatus ed ON up.EducationStatusId = ed.Id
-                    LEFT JOIN SexualOrientation so ON up.SexualOrientationId = so.Id
-                    LEFT JOIN Religion r ON up.ReligionId = r.Id
-                    LEFT JOIN Ethnicity e ON up.EthnicityId = e.Id
-                    LEFT JOIN HaveKids hk ON up.HaveKidsId = hk.Id
-                    LEFT JOIN WantKids wk ON up.WantKidsId = wk.Id
-                    LEFT JOIN Location loc ON up.LocationId = loc.Id
-                    WHERE up.UserId != @userId
-                    AND EXISTS 
+                SET @from_row = ((@pageNumber * @pageSize) - @pageSize) + 1;
+            END;
+
+            WITH UserProfileRecords AS
+            (
+                SELECT 
+                    up.UserId, 
+                    up.DOB, 
+                    g.Id AS GenderId, 
+                    g.GenderType, 
+                    oc.Id AS OccupationId,
+                    oc.OccupationName,
+                    up.IsOccupationVisible,
+                    ed.Id AS EducationStatusId,
+                    ed.EducationLevel,
+                    up.IsEducationStatusVisible,
+                    so.Id AS SexualOrientationId, 
+                    so.SexualOrientationType, 
+                    up.IsSexualOrientationVisible, 
+                    r.Id AS ReligionId, 
+                    r.ReligionType, 
+                    up.IsReligionVisible, 
+                    e.Id AS EthnicityId, 
+                    e.EthnicityType, 
+                    up.IsEthnicityVisible, 
+                    hk.Id AS HaveKidsId, 
+                    hk.HaveKidsType, 
+                    wk.Id AS WantKidsId, 
+                    wk.WantKidsType, 
+                    loc.Id AS LocationId, 
+                    loc.Lat, 
+                    loc.Lng, 
+                    up.AllowNotifications,
+                    up.Passions,
+                    up.ProfileTypes,
+                    up.DateCreated,
+                    up.DateModified,
+                    up.IsDeleted,
+                    up.Id,
                     (
-                        SELECT 1 FROM OPENJSON(@profileTypes) AS jsonProfileTypes
-                        WHERE up.ProfileTypes LIKE '%' + jsonProfileTypes.value + '%'
-                    )
-                    AND (@filterByDating IS NULL OR 
-                        (up.ProfileTypes LIKE '%"" + CAST(0 AS NVARCHAR(MAX)) + @""%'))
-                    AND (@filterByFriendship IS NULL OR 
-                        (up.ProfileTypes LIKE '%"" + CAST(1 AS NVARCHAR(MAX)) + @""%'))
-                ),
-                RecordCount AS
+                        SELECT 
+                            pr.Id,
+                            pr.ResponseType,
+                            pr.Response,
+                            pr.PromptQuestionId
+                        FROM dbo.PromptResponse pr
+                        WHERE pr.UserProfileId = up.Id
+                        FOR JSON PATH
+                    ) AS PromptResponses,
+                    ROW_NUMBER() OVER (ORDER BY up.DateCreated DESC) AS Row_Num
+                FROM dbo.UserProfiles up
+                LEFT JOIN dbo.Genders g ON up.GenderId = g.Id
+                LEFT JOIN dbo.Occupation oc ON up.OccupationId = oc.Id
+                LEFT JOIN dbo.EducationStatus ed ON up.EducationStatusId = ed.Id
+                LEFT JOIN dbo.SexualOrientation so ON up.SexualOrientationId = so.Id
+                LEFT JOIN dbo.Religion r ON up.ReligionId = r.Id
+                LEFT JOIN dbo.Ethnicity e ON up.EthnicityId = e.Id
+                LEFT JOIN dbo.HaveKids hk ON up.HaveKidsId = hk.Id
+                LEFT JOIN dbo.WantKids wk ON up.WantKidsId = wk.Id
+                LEFT JOIN dbo.Location loc ON up.LocationId = loc.Id
+                WHERE up.UserId != @userId
+                AND EXISTS 
                 (
-                    SELECT COUNT(*) AS TotalCount FROM UserProfileRecords
+                    SELECT 1 
+                    FROM OPENJSON(@profileTypes) AS jsonProfileTypes
+                    WHERE up.ProfileTypes LIKE '%' + jsonProfileTypes.value + '%'
                 )
-            
-                SELECT * 
+                AND (@filterByDating IS NULL OR 
+                     (up.ProfileTypes LIKE '%""' + CAST(0 AS NVARCHAR(MAX)) + '""%'))
+                AND (@filterByFriendship IS NULL OR 
+                     (up.ProfileTypes LIKE '%""' + CAST(1 AS NVARCHAR(MAX)) + '""%'))
+            ),
+            RecordCount AS
+            (
+                SELECT COUNT(*) AS TotalCount 
                 FROM UserProfileRecords
-                CROSS JOIN RecordCount
-                WHERE Row_Num BETWEEN @from_row AND (@from_row + @pageSize - 1)
-                ORDER BY DateCreated DESC;
-             END
-");
+            )
+            SELECT * 
+            FROM UserProfileRecords
+            CROSS JOIN RecordCount
+            WHERE Row_Num BETWEEN @from_row AND (@from_row + @pageSize - 1)
+            ORDER BY DateCreated DESC;
+        END;
+        ");
         }
 
         /// <inheritdoc />
